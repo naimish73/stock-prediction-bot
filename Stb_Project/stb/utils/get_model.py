a39c27335aca73fa3be27cf53deb9583d95b2806
Rjchauhan18 <rjchauhan5000000@gmail.com>
2024-04-10 22:29:12 +0530

    created 5m_nifty_model_testing jupiter notebook and created randome forest,decision tree and Xgboost model (which are working for only one symbol)

--- Stb_Project/stb/utils/get_model.py
+++ Stb_Project/stb/utils/get_model.py
@@ -1,39 +1,61 @@
 import joblib
 import yfinance as yf
 import plotly.graph_objects as go
-
+import pickle
 
 # Load the model from .pkl file
 # model = joblib.load('../../technical_analysis_model/model_pkl_files/decision_tree_model.pkl')
-model = joblib.load('/home/rahul/Desktop/Rahul/DE SEM 6/stock-prediction-bot/technical_analysis/technical_analysis_model/model_pkl_files/decision_tree_model.pkl')
+# model = joblib.load('/home/rahul/Desktop/Rahul/DE SEM 6/stock-prediction-bot/technical_analysis/technical_analysis_model/model_pkl_files/decision_tree_model.pkl')
+
+# Load the model from .pkl file
+
+# Load the model from the pickle file
+with open('/home/rahul/Desktop/Rahul/DE SEM 6/stock-prediction-bot/technical_analysis/technical_analysis_model/model_pkl_files/RandomForestRegressor.pkl', 'rb') as f:
+    Randomforest = pickle.load(f)
+
+
+with open('/home/rahul/Desktop/Rahul/DE SEM 6/stock-prediction-bot/technical_analysis/technical_analysis_model/model_pkl_files/DesicionTreeModel.pkl', 'rb') as f:
+    DecisionTree = pickle.load(f)
+
+
+with open('/home/rahul/Desktop/Rahul/DE SEM 6/stock-prediction-bot/technical_analysis/technical_analysis_model/model_pkl_files/XGBoost.pkl', 'rb') as f:
+    XgBoostModel = pickle.load(f)
+
+
+
+
 
 # Function to fetch latest data and make predictions
-def fetch_data_and_predict(symbol,model_title,time_frame='1m'):
+def XgBoost_predict(symbol,time_frame='1m'):
     # Fetch latest data
     ticker = yf.Ticker(symbol)
     data = ticker.history(interval=time_frame, period="1d")
-    data.drop(columns=['Volume', 'Dividends', 'Stock Splits'], inplace=True)
+    data.reset_index(inplace=True)
+    data.drop(columns=['Datetime','Volume', 'Dividends', 'Stock Splits'], inplace=True)
 
-    # Prepare data for prediction
-    X = data[['Open', 'High', 'Low', 'Close']]
+    # # Prepare data for prediction
+    # X = data[['Open', 'High', 'Low', 'Close']]
 
     # Predict using the model
-    live_prediction = model.predict(X)
+    XgBoostModel_pred = XgBoostModel.predict(data)
 
     # C lear previous graph
     # clear_output(wait=True)
 
     t1 = go.Scatter(x=data.index, y=data['Close'], mode='lines', name="Actual Close", line=dict(color='blue'))
-    t2 = go.Scatter(x=data.index, y=live_prediction, mode='lines', name="Predicted Close", line=dict(color='red'))
+    t2 = go.Scatter(x=data.index, y=XgBoostModel_pred, mode='lines', name="Predicted Close", line=dict(color='red'))
 
     # Create layout for the plot
-    layout = go.Layout(title=f'Stock Price Prediction With {model_title}', xaxis=dict(title='Date'), yaxis=dict(title='Price'))
+    layout = go.Layout(title=f'Stock Price Prediction With XgBoost Model', xaxis=dict(title='Date'), yaxis=dict(title='Price'))
 
     # Combine traces and layout into a figure
     fig = go.Figure(data=[t1, t2], layout=layout)
+    fig.show()
 
-    return fig
-    """# Plotting
+    # return fig
+XgBoost_predict('ITC.NS')
+
+"""# Plotting
     plt.figure(figsize=(10, 6))
     plt.plot(data.index, data['Close'], label='Actual Close', color='blue')
     plt.plot(data.index, live_prediction, label='Predicted Close', color='red', linestyle='dashed')
